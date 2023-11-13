db0f98ff02a4bcaa98437b0e2f2e0ad0f0cf4f86
Leejo Jose <leejojosepj@gmail.com>
2023-11-13 20:13:06 +0530

    test commit crlf

---
+++ src/main/java/com/clrftest/ClrftestSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.clrftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClrftestSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClrftestSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clrftest/ClrftestSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clrftest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClrftestSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
